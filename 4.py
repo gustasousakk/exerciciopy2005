45730a0ec35092309b8142ab76db1b3ad42d0359
gustasousakk <161743684+gustasousakk@users.noreply.github.com>
2024-08-22 16:42:06 -0300

    fórmula de bhaskara

--- 4.py
+++
@@ -1,9 +0,0 @@
-#exercicio4
-a=int(input("valor de a="))
-b=int(input("valor de b="))
-c=int(input("valor de c="))
-delta=b**2-4*a*c
-x1=((-b)+delta**(1/2))/(2*a)
-x2=((-b)-delta**(1/2))/(2*a)
-print(x1)
-print(x2)
