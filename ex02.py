ca812509e74de522060d820a69566418a89852d7
gustasousakk <161743684+gustasousakk@users.noreply.github.com>
2024-08-22 16:56:35 -0300

    Add files via upload

---
+++ ex02.py
@@ -0,0 +1,7 @@
+
+#exercicio02
+
+horas= int(input("horas:"))
+minutos= int(input("minutos:"))
+minutosTotais = (horas*60)+minutos
+print(minutosTotais, "minutos totais")
---
+++ ex03.py
@@ -0,0 +1,7 @@
+
+#exercicio03
+anos = int(input("anos:"))
+meses= int(input("meses:"))
+dias = int(input("dias:"))
+idadeDias = (365*anos)+(30*meses)+(24*dias)
+print(idadeDias, "dias totais")
